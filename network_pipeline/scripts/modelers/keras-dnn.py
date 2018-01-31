bba5f47ca5ffc6e6207c05856adf6943429e8e8c
jay-johnson <jay.p.h.johnson@gmail.com>
2018-01-31 01:58:20 -0800

    fix for ignoring non-numeric features by reading the meta data header file before starting the keras model fit.

--- network_pipeline/scripts/modelers/keras-dnn.py
+++ network_pipeline/scripts/modelers/keras-dnn.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import logging
+import json
 import numpy as np
 import pandas as pd
 from celery_connectors.utils import ev
@@ -25,11 +26,18 @@ np.random.seed(seed)
 csv_file = ev(
     "CSV_FILE",
     "/tmp/merge_only_attack_scans.csv")
+meta_file = ev(
+    "CSV_META_FILE",
+    "/tmp/merge_headers.json")
 
 if not os.path.exists(csv_file):
     log.error(("missing csv_file={}")
               .format(csv_file))
     sys.exit(1)
+
+predict_feature = ev(
+    "PREDICT_FEATURE",
+    "label_value")
 
 log.info(("Loading csv={}")
          .format(csv_file))
@@ -44,16 +52,54 @@ test_size = float(ev(
 train_size = 1.0 - test_size
 
 feature_to_process = []
+ignore_features = [
+    "label_name",
+    "ip_src",   # need to make this an int
+    "ip_dst",   # need to make this an int
+    "eth_src",  # need to make this an int
+    "eth_dst"   # need to make this an int
+]
+meta_data = {}
 
-for f in df.columns.values:
-    if f == "label_value":
-        feature_to_process.append(f)
-# end of for all columns
+try:
+    meta_data = json.loads(
+        open(meta_file, "r").read()
+    )
+    if "post_proc_rules" in meta_data:
+        if "drop_columns" in meta_data["post_proc_rules"]:
+            for ign in meta_data["post_proc_rules"]["drop_columns"]:
+                ignore_features.append(ign)
+except Exception as e:
+    log.error(("Failed building ignore_features: "
+               "ignore_features={} meta={} meta_data={} "
+               "ex={}")
+              .format(ignore_features,
+                      meta_file,
+                      meta_data,
+                      e))
+# end of trying to lookup the meta data file
+# for non-int/float features to ignore
+
+for feature in df.columns.values:
+    keep_it = True
+    for ign in ignore_features:
+        if feature == ign:
+            keep_it = False
+    if keep_it:
+        feature_to_process.append(feature)
+# end of for all features to process
+
+log.info(("Predicting={} with features={} "
+          "ignore_features={} records={}")
+         .format(predict_feature,
+                 feature_to_process,
+                 ignore_features,
+                 len(df.index)))
 
 # split the data into training
 (X_train, X_test, Y_train, Y_test) = \
         train_test_split(df[feature_to_process],
-                         df["label_value"],
+                         df[predict_feature],
                          test_size=test_size,
                          random_state=seed)
 
