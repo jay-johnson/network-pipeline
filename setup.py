0259e9a3a39036c50e086128089c2d797af663ce
jay-johnson <jay.p.h.johnson@gmail.com>
2018-06-24 11:12:22 -0700

    new container for base update

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "network_pipeline"))
 setup(
     name="network-pipeline",
     cmdclass={"build_py": build_py},
-    version="1.0.32",
+    version="1.0.33",
     description="Distributed Network Packet Analysis Pipeline " +
     "for Layer 2, 3 and 4 Frames",
     long_description="" +
