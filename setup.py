4e53ae13fe12085e0cf2e5e1aff947368f4f1ffa
jay-johnson <jay.p.h.johnson@gmail.com>
2018-11-02 04:08:15 +0000

    version bump for container

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "network_pipeline"))
 setup(
     name="network-pipeline",
     cmdclass={"build_py": build_py},
-    version="1.2.9",
+    version="1.2.10",
     description=(
         "Distributed Network Packet Analysis Pipeline " +
         "for Layer 2, 3 and 4 Frames"),
